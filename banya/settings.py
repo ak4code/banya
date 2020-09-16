60cf909b6a04e9ef1b7f9967d2c689b2992e07f3
akweb <haritonov.aka@gmail.com>
2020-09-17 02:11:50 +0300

    feat: celery

--- banya/settings.py
+++ banya/settings.py
@@ -140,6 +140,3 @@ EMAIL_PORT = 587
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TASK_SERIALIZER = 'json'
