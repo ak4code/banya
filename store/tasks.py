9fe8b85a6450e7af7ff2af326fc246af5314e576
akweb <haritonov.aka@gmail.com>
2020-09-17 23:18:48 +0300

    feat: celery

--- store/tasks.py
+++ store/tasks.py
@@ -7,7 +7,7 @@ from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.html import strip_tags
 
-from banya.celery import app
+from banya.celeryapp import app
 
 
 @app.task
