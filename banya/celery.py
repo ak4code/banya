fdbd865b752de2c3220f8297233873b173a28b00
akweb <haritonov.aka@gmail.com>
2020-09-21 12:30:04 +0300

    feat: celery

--- banya/celery.py
+++ banya/celery.py
@@ -1,9 +1,11 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banya.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banya.settings.prod')
 
 app = Celery('banya')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
