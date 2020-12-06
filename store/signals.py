fbad4c1b3b0a3f009b43d79c4d3e13609337a1be
kharitonov.dev <haritonov.aka@gmail.com>
2020-12-06 19:19:57 +0300

    git

--- store/signals.py
+++ store/signals.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+
 from store.tasks import send_registration_email
 
 
 def user_post_save(sender, instance, created, *args, **kwargs):
+    if settings.DEBUG:
+        return
     if created:
         send_registration_email.delay(instance.pk)
